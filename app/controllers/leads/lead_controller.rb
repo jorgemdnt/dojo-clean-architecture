615fce193435e5462baa66269b9401eb156354e0
marco-pontes <marco.pontes@resultadosdigitais.com.br>
2020-06-26 17:21:23 -0300

    Called the use case in the controller

--- app/controllers/leads/lead_controller.rb
+++ app/controllers/leads/lead_controller.rb
@@ -3,7 +3,12 @@ require 'leads/usecases/create_lead_usecase'
 module Leads
   class LeadController < ApplicationController
     def create
+      lead_gateway = Leads::LeadActiveRecordGateway.new
+      create_lead_presenter = Leads::CreateLeadPresenter.new
       use_case = Leads::Usecases::CreateLeadUsecase.new(lead_gateway, create_lead_presenter)
+      new_lead = Entities::Lead.new(name: params[:name], email: params[:email], phone: params[:phone])
+      use_case.run(new_lead)
+      render create_lead_presenter.make_response
     end
   end
 end
--- spec/controllers/leads/lead_controller_spec.rb
+++ spec/controllers/leads/lead_controller_spec.rb
@@ -3,4 +3,23 @@ require 'leads/entities/lead'
 
 RSpec.describe Leads::LeadController do
 
+  describe 'POST #create' do
+    before do
+      post :create, params: {email: 'example@example.com',
+                             name: 'Foo',
+                             phone: '99 99999 9999'}
+    end
+
+    it 'returns response status 201' do
+      expect(response).to have_http_status(201)
+    end
+
+    it 'returns response status 201' do
+      expect(JSON.parse(response.body)["id"]).to be_kind_of(Integer)
+      expect(JSON.parse(response.body)["name"]).to eq('Foo')
+      expect(JSON.parse(response.body)["email"]).to eq('example@example.com')
+      expect(JSON.parse(response.body)["phone"]).to eq('99 99999 9999')
+    end
+  end
+
 end
