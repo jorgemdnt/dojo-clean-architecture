68db511871c532966395c883e992847b7549294d
marco-pontes <marco.pontes@resultadosdigitais.com.br>
2020-06-26 14:12:59 -0300

    Creating entity for leads

---
+++ lib/leads/entities/lead.rb
@@ -0,0 +1,14 @@
+module Leads
+  module Entities
+    class Lead
+      attr_reader :id, :email, :name, :phone
+
+      def initialize(id:, email:, name:, phone:)
+        @id = id
+        @email = email
+        @name = name
+        @phone = phone
+      end
+    end
+  end
+end
